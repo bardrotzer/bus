746a4d1cfe93cfc054f1f8416ac5c1c737434046
Bård Røtzer <bard@kartoteket.as>
2019-04-17 11:43:39 +0300

    create endpoint for app

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import express from "express";
+import router from "./routes/index";
+import dotenv from 'dotenv';
+// load the dotenv lib
+dotenv.config();
+
+// start express
+const app = express();
+
+// set datatype to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set app to accept any incoming
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, application/json");
+  res.header("Content-Type", "application/json")
+  next();
+});
+
+// use the router defined in routes/index,js
+app.use("/", router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Bus running on ', port);
+});
